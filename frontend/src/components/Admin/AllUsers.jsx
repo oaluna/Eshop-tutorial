f70b66539f84008556099f1041228dcc74fde2e3
Oscar Luna <55468657+oaluna@users.noreply.github.com>
2024-05-08 16:33:42 +0000

    "Updated Package versions"

--- frontend/src/components/Admin/AllUsers.jsx
+++ frontend/src/components/Admin/AllUsers.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { getAllUsers } from "../../redux/actions/user";
-import { DataGrid } from "@material-ui/data-grid";
+import { DataGrid } from "@mui/x-data-grid";
 import { AiOutlineDelete } from "react-icons/ai";
 import { Button } from "@material-ui/core";
 import styles from "../../styles/styles";
